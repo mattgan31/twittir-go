1e9f5d8cb462900933f470b228a181ac8cb414d8
mattgan31 <alamsyahr00@gmail.com>
2023-09-15 19:10:25 +0700

    add column deletedAt for soft delete, add feature delete post

--- models/gormModel.go
+++ models/gormModel.go
@@ -1,9 +1,14 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type GormModel struct {
-	ID         uint      `gorm:"primaryKey" json:"id"`
-	Created_At time.Time `json:"createdAt,omitempty"`
-	Updated_At time.Time `json:"updated_at,omitempty"`
+	ID         uint           `gorm:"primaryKey" json:"id"`
+	Created_At time.Time      `json:"createdAt,omitempty"`
+	Updated_At time.Time      `json:"updatedAt,omitempty"`
+	Deleted_At gorm.DeletedAt `gorm:"index"`
 }
